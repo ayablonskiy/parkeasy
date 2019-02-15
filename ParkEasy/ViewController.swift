ae2ae90bbbddcc6575985530a0f7bad82970acb4
Aleksei <a.yablonskiy@se.qmul.ac.uk>
2019-02-15 19:55:50 +0000

    Initial Commit

---
+++ ParkEasy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParkEasy
+//
+//  Created by Yablonskiy Alexey on 2/15/19.
+//  Copyright © 2019 Yablonskiy Alexey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
